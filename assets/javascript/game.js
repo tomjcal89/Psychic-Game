a57a0c048aa6c9584ca8f50ed543d2fca669653f
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-05 22:20:19 -0700

    added changes to java script and html. defined variables

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,30 @@
 // establish vairables
 
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i",
+"j", "k", "l", "m", "n", "o", "p", "q", "r",
+"s", "t", "u", "v", "w", "x", "y", "z"];
+var lost = 0;
+var win = 0;
+var guesses = 9;
+var guessLetter = "";
+
 // pull elements by id
+var winning = document.getElementById("wins");
+var losing = document.getElementById("losses");
+var guess = document.getElementById("guessesLeft");
+var letterGuess = document.getElementById("lettersGuessed");
+
+//setting scores
+winning.textContent = win;
+losing.textContent = lost;
+guess.textContent = guesses;
+letterGuess.textContent = guessLetter
 
 //have computer come up with random letter
+
+var randLetter = letters[Math.floor(Math.random() * letters.length)];
+console.log(randLetter)
+
 
 //when you press an incorrect key
     //have letter show up in "letters you have guessed"
