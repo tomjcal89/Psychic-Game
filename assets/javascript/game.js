ffe6cd2b49e9c6d00a65e7153f02226016a96cba
Thomas J Calhoun <thomascalhoun@Thomass-MBP.attlocal.net>
2018-10-05 21:01:18 -0700

    initial commit, files and started html code

---
+++ assets/javascript/game.js
@@ -0,0 +1,20 @@
+// establish vairables
+
+// pull elements by id
+
+//have computer come up with random letter
+
+//when you press an incorrect key
+    //have letter show up in "letters you have guessed"
+        //have the "number of Guesses left go down by 1"
+
+
+// if number of guesses equal 0, add 1 to number of losses
+
+//if number of guesses equal 0, have game restart
+    //number of guesses reset to 9
+    //guessed letters reset to nothing
+
+// if correct key is hit, number of wins goes up by 1
+    //letters guessed resets
+    //number of guesses left resets to 9
